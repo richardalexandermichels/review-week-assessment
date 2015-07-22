84ca17c8b050d6acf88bd0fec145cd97854f0653
richardalexandermichels <richard.alexandermichels@gmail.com>
2015-07-22 16:02:24 -0400

    student solutions

--- models/task.model.js
+++ models/task.model.js
@@ -15,26 +15,43 @@ var TaskSchema = new Schema({
 //virtuals
 
 TaskSchema.virtual('timeRemaining').get(function() {
-  
+  if(!this.due){return Infinity}
+  return this.due - Date.now()
 })
 
 TaskSchema.virtual('overdue').get(function() {
+	if(this.due - Date.now()<0){
+		return true
+	}return false
 
 })
 
 //methods
 
 TaskSchema.methods.addChild = function(params) {
- 
+ 	params.parent = this._id
+ 	return this.constructor.create(params)
 }
 
 TaskSchema.methods.getChildren = function() {
-
+	return this.constructor.find({parent:this._id}).exec()
 }
 
 TaskSchema.methods.getSiblings = function() {
-
+	var self = this
+	return this.constructor.find({parent:this.parent}).exec()
+		.then(function(siblings){
+			var not_me = [];
+			siblings.forEach(function(ele){
+				if(JSON.stringify(ele._id) !== JSON.stringify(self.id)){
+					not_me.push(ele)
+				}
+			})
+			return not_me;
+		})
 }
+
+
 
 Task = mongoose.model('Task', TaskSchema);
 
